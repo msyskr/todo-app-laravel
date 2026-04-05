7810a073c4f7179e32a830cf37fb5007bcd5672d
msyskr <msy.skr.0332@gmail.com>
2026-04-05 15:40:14 +0900

    スクショ追加

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -11,7 +11,7 @@
 <body class="bg-gray-100">
 
     <div class="container mx-auto p-4 md:w-6/12">
-        <nav class="flex justify-between md:justify-normal">
+        <nav class="flex justify-between items-center">
             <h1 class="text-2xl font-bold mb-4">Todoリスト</h1>
             <a href="{{route('tasks.trash') }}" class="bg-gray-300 p-2 mb-2 rounded">ゴミ箱</a>
         </nav>
@@ -28,9 +28,9 @@
 
                 <div>
                     <div>
-                        <label for="due_time" class="text-xs">期限</label>
+                        <label for="due_date" class="text-xs">期限</label>
                     </div>
-                    <input type="datetime-local" name="due_time" value="{{now()->format('Y-m-d H:i')}}" class="border border-gray-300 p-2 w-full">
+                    <input type="datetime-local" name="due_date" value="{{now()->format('Y-m-d\TH:i')}}" class="border border-gray-300 p-2 w-full">
                 </div>
                 <button type="submit" class="bg-blue-500 text-white p-2 mt-2 rounded">追加</button>
             </div>
@@ -41,7 +41,7 @@
         @foreach ($tasks as $task)
             <li class="flex justify-between mb-2">
                 <div>{{ $task->task_name }}:
-                    <span class="mx-2">{{date('Y-m-d H:i',
+                    <span class="mx-2">{{date('Y-m-d\TH:i',
                     strtotime($task->due_date))}}</span></div>
 
                 <form action="{{route('tasks.markAsDeleted', $task->id)}}" method="POST">
