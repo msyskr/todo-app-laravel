a075f8538df5e6ac034ad72085a850ac3601a2b7
msyskr <msy.skr.0332@gmail.com>
2026-04-11 13:04:58 +0900

    force load css

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todoリスト</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="/build/assets/app-DvAD4qSo.css">
 </head>
 <body class="bg-gray-100">
 
